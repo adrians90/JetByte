07f0c422b63b1318e74831059ab929d31b9736d7
adrians90 <adriansandu9090@gmail.com>
2024-07-18 14:10:43 +0100

    feat: Add Sidebar component and update Sidebar.jsx, MemberProfile.jsx, NavLinks.jsx, and SidebarHeader.jsx

--- jetbyte/components/Sidebar.jsx
+++ jetbyte/components/Sidebar.jsx
@@ -1,5 +1,15 @@
+import MemberProfile from "./MemberProfile";
+import NavLinks from "./NavLinks";
+import SidebarHeader from "./SidebarHeader";
+
 const Sidebar = () => {
-  return <div className="text-6xl text-slate-50">Sidebar</div>;
+  return (
+    <div className="text-slate-50 px-4 w-80 min-h-full bg-slate-900 py-12 grid grid-rows-[auto,1fr,auto]">
+      <SidebarHeader />
+      <NavLinks />
+      <MemberProfile />
+    </div>
+  );
 };
 
 export default Sidebar;
