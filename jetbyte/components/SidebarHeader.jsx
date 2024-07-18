6ec92044d62df773149858137e7280c7834e46ef
adrians90 <adriansandu9090@gmail.com>
2024-07-18 14:21:05 +0100

    chore: Update SidebarHeader and ThemeToggle components

--- jetbyte/components/SidebarHeader.jsx
+++ jetbyte/components/SidebarHeader.jsx
@@ -1,5 +1,16 @@
-function SidebarHeader() {
-  return <div>SidebarHeader</div>;
-}
+import { SiOpenaigym } from "react-icons/si";
+import ThemeToggle from "./ThemeToggle";
+
+const SidebarHeader = () => {
+  return (
+    <div className="flex items-center mb-4 gap-4 px-4">
+      <SiOpenaigym className="w-20 h-20 text-lime-500" />
+      <h2 className="text-xl font-extrabold text-slate-50">
+        Jet<span className="text-lime-500 italic">Byte</span>
+      </h2>
+      <ThemeToggle />
+    </div>
+  );
+};
 
 export default SidebarHeader;
