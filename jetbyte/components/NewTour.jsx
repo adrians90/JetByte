65bef910528eb6f779baf80d787990f9ce03a92e
adrians90 <adriansandu9090@gmail.com>
2024-07-19 15:40:30 +0100

    feat: Add form for selecting dream destination in NewTour component

--- jetbyte/components/NewTour.jsx
+++ jetbyte/components/NewTour.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+
+import TourInfo from "./TourInfo";
 
 const NewTour = () => {
-  return <div>NewTour</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="max-w-2xl">
+        <h2 className="mb-4 font-bold text-slate-50/90">
+          Select your dream destination
+        </h2>
+
+        <div className="join w-full">
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="City"
+            name="city"
+            required
+          ></input>
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="Country"
+            name="country"
+            required
+          ></input>
+          <button className="btn text-slate-950 font-bold transition:all hover:bg-fuchsia-400 bg-fuchsia-500 join-item">
+            Generate Tour
+          </button>
+        </div>
+      </form>
+      <div className="mt-16">
+        <TourInfo />
+      </div>
+    </>
+  );
 };
 
 export default NewTour;
