0171ba704a8c54ba85e518aa7990ae70b72a5a5a
adrians90 <adriansandu9090@gmail.com>
2024-07-22 11:11:52 +0100

    feat: Add close button to sidebar header

--- jetbyte/components/SidebarHeader.jsx
+++ jetbyte/components/SidebarHeader.jsx
@@ -1,4 +1,5 @@
 import { SiOpenaigym } from "react-icons/si";
+import { IoClose } from "react-icons/io5";
 
 const SidebarHeader = () => {
   return (
@@ -7,6 +8,15 @@ const SidebarHeader = () => {
       <h2 className="text-2xl md:text-3xl font-extrabold text-slate-50">
         Jet<span className="text-fuchsia-500 italic">Byte</span>
       </h2>
+      <div>
+        <input type="checkbox" id="my-drawer-2" className="drawer-toggle" />
+        <label
+          htmlFor="my-drawer-2"
+          className="drawer-button lg:hidden fixed top-4 right-4 cursor-pointer text-lg text-fuchsia-500 hover:text-fuchsia-400"
+        >
+          <IoClose className="cursor-pointer text-3xl text-fuchsia-500 hover:text-fuchsia-400" />
+        </label>
+      </div>
     </div>
   );
 };
