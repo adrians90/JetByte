9ca2329a2d1dce45db963079750e1ad274d8d7e9
adrians90 <adriansandu9090@gmail.com>
2024-07-22 14:55:16 +0100

    feat: Update tour page to fetch tour image from Unsplash API

--- jetbyte/app/(dashboard)/tours/[id]/page.js
+++ jetbyte/app/(dashboard)/tours/[id]/page.js
@@ -3,6 +3,8 @@ import { generateTourImage, getSingleTour } from "@/utils/actions";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import axios from "axios";
+const url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=`;
 
 const SingleTourPage = async ({ params }) => {
   const tour = await getSingleTour(params.id);
@@ -11,10 +13,13 @@ const SingleTourPage = async ({ params }) => {
     redirect("/tours");
   }
 
-  const tourImage = await generateTourImage({
-    city: tour.city,
-    country: tour.country,
-  });
+  const { data } = await axios.get(`${url}${tour.city}`);
+  const tourImage = data?.results[0]?.urls?.raw;
+
+  //   const tourImage = await generateTourImage({
+  //     city: tour.city,
+  //     country: tour.country,
+  //   });
   return (
     <div>
       <Link href="/tours" className="btn btn-secondary mb-12">
