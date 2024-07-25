39c0dddb3d50ebc9063b30513c436f585888d805
adrians90 <adriansandu9090@gmail.com>
2024-07-25 13:22:38 +0100

    feat: Add link to homepage in SidebarHeader component

--- jetbyte/components/SidebarHeader.jsx
+++ jetbyte/components/SidebarHeader.jsx
@@ -1,13 +1,16 @@
 import { SiOpenaigym } from "react-icons/si";
 import { IoClose } from "react-icons/io5";
+import Link from "next/link";
 
 const SidebarHeader = () => {
   return (
     <div className="flex items-center mb-4 gap-4 px-4">
       <SiOpenaigym className="w-10 h-20 text-fuchsia-500" />
-      <h2 className="text-2xl md:text-3xl font-extrabold text-slate-50">
-        Jet<span className="text-fuchsia-500 italic">Byte</span>
-      </h2>
+      <Link href="/">
+        <h2 className="text-2xl md:text-3xl font-extrabold text-slate-50">
+          Jet<span className="text-fuchsia-500 italic">Byte</span>
+        </h2>
+      </Link>
       <div>
         <input type="checkbox" id="my-drawer-2" className="drawer-toggle" />
         <label
